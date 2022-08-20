1d1a866e32bd9e630730373daeae92cbe4545a8c
reosfire <2.com9761721@gmail.com>
2022-08-20 22:53:20 +0300

    get uuid

---
+++ src/main/java/ru/reosfire/wmlib/extensions/java/ResultSetExtensions.kt
@@ -0,0 +1,21 @@
+package ru.reosfire.wmlib.extensions.java
+
+import ru.reosfire.wmlib.utils.UUIDConverter
+import java.sql.ResultSet
+import java.util.UUID
+
+fun ResultSet.getUUID(columnIndex: Int): UUID {
+    return UUID.fromString(getString(columnIndex))
+}
+
+fun ResultSet.getUUID(columnLabel: String): UUID {
+    return UUID.fromString(getString(columnLabel))
+}
+
+fun ResultSet.getUUIDFromBytes(columnIndex: Int): UUID {
+    return UUIDConverter.fromBytes(getBytes(columnIndex))
+}
+
+fun ResultSet.getUUIDFromBytes(columnLabel: String): UUID {
+    return UUIDConverter.fromBytes(getBytes(columnLabel))
+}
