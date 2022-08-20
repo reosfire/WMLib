1f05b5f2f028fcdaa27efa027abed7b36051f323
reosfire <2.com9761721@gmail.com>
2022-08-20 23:33:13 +0300

    register command

--- src/main/java/ru/reosfire/wmlib/extensions/PluginExtensions.kt
+++ src/main/java/ru/reosfire/wmlib/extensions/PluginExtensions.kt
@@ -1,7 +1,10 @@
 package ru.reosfire.wmlib.extensions
 
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.TabCompleter
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
 import ru.reosfire.wmlib.yaml.IConfigCreator
 import ru.reosfire.wmlib.yaml.YamlConfig
 import java.io.File
@@ -9,6 +12,12 @@ import java.nio.file.Path
 
 fun Plugin.registerListener(listener: Listener) {
     this.server.pluginManager.registerEvents(listener, this)
+}
+
+fun JavaPlugin.registerCommand(name: String, executor: CommandExecutor) {
+    val pluginCommand = getCommand(name)
+    pluginCommand.executor = executor
+    if (executor is TabCompleter) pluginCommand.tabCompleter = executor
 }
 
 fun<T: YamlConfig?> Plugin.loadConfig(creator: IConfigCreator<T>): T {
